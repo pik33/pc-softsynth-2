1a18eeac83d7cd8155fa8118dabe6636de5db816
Piotr Kardasz <pik33@o2.pl>
2026-01-30 16:46:18 +0100

    20260130

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -5,7 +5,7 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,sdl3;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, sdl3;
 
 type
 
@@ -22,6 +22,9 @@ type
 
 var
   Form1: TForm1;
+  window: PSDL_Window;
+  renderer: PSDL_Renderer;
+
 
 implementation
 
@@ -30,8 +33,25 @@ implementation
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
+
+var WindowSize: TSDL_Point = (x: 1024; y: 600);
+
 begin
-  form1.color:=$FF;
+
+if not SDL_Init(SDL_INIT_VIDEO or SDL_INIT_AUDIO or SDL_INIT_EVENTS) then
+  exit;
+
+
+window := SDL_CreateWindow('SDL3 Window', WindowSize.x, WindowSize.y, SDL_WINDOW_RESIZABLE);
+if window = nil then begin
+end;
+
+renderer := SDL_CreateRenderer(window, nil);
+if renderer = nil then begin
+end;
+
+end;
+
 end;
 
 end.
--- unit1.pas
+++ unit1.pas
@@ -22,6 +22,9 @@ type
 
 var
   Form1: TForm1;
+  window: PSDL_Window;
+  renderer: PSDL_Renderer;
+
 
 implementation
 
@@ -30,9 +33,26 @@ implementation
 { TForm1 }
 
 procedure TForm1.Button1Click(Sender: TObject);
+
+var WindowSize: TSDL_Point = (x: 1024; y: 600);
+
 begin
-  form1.color:=$FF;
+
+if not SDL_Init(SDL_INIT_VIDEO or SDL_INIT_AUDIO or SDL_INIT_EVENTS) then
+  exit;
+
+
+window := SDL_CreateWindow('SDL3 Window', WindowSize.x, WindowSize.y, SDL_WINDOW_RESIZABLE);
+if window = nil then begin
 end;
+
+renderer := SDL_CreateRenderer(window, nil);
+if renderer = nil then begin
+end;
+
+end;
+
+
 
 end.
 
